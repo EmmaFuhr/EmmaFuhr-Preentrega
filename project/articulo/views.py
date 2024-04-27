52b4b3374a20efc846653f2df6b3d38e5519d770
Emma Fuhr <emma.a.fuhr@gmail.com>
2024-04-27 19:07:39 -0300

    add: creacion templates en articulo y configuramos navegacion

--- project/articulo/views.py
+++ project/articulo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+def home(request):
+    consulta_articulos = models.Articulo.objects.all()
+    context = {"articulos": consulta_articulos}
+    return render(request, "articulo/index.html", context)
