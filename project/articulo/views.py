1695d9169be15e0f4a0918ebdf9103e3f44e5ac6
Emma Fuhr <emma.a.fuhr@gmail.com>
2024-05-16 11:54:18 -0300

    fix: solucion error al cargar template update

--- project/articulo/views.py
+++ project/articulo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import CreateView,DeleteView, DetailView, ListView, UpdateView
 from . import models, forms
 from django.urls import reverse_lazy
+from django.db.models import Q
 # Create your views here.
 def home(request):
     return render(request, "articulo/index.html")
@@ -9,6 +10,17 @@ def home(request):
 class ArticuloList(ListView):
     model = models.Articulo
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        consulta = self.request.GET.get('consulta')  
+
+        if consulta:  
+            queryset = queryset.filter(
+                Q(nombre__icontains=consulta) |  
+                Q(descripcion__icontains=consulta)  
+            )
+        return queryset
+    
 class ArticuloDetail(DetailView):
     model = models.Articulo
 
@@ -24,7 +36,10 @@ class ArticuloCreate(CreateView):
 class ArticuloUpdate(UpdateView):
     model = models.Articulo
     form_class = forms.ArticuloForm
+    template_name = "articulo/articulo_update.html"
     success_url = reverse_lazy("articulo:articulo_list")
+
+
 
 
 
