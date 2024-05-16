836327a4bbd192f090f60a645b9a2d5c64728e3d
Emma Fuhr <emma.a.fuhr@gmail.com>
2024-05-16 11:55:17 -0300

    add: CRUD con Clases para Cliente

--- project/cliente/urls.py
+++ project/cliente/urls.py
@@ -5,7 +5,11 @@ app_name = 'cliente'
 
 urlpatterns = [
     path("cliente/", views.home, name="home"),
-    path("crear_cliente/", views.crear_cliente, name="crear_cliente"),
+    path("cliente/create/", views.ClienteCreate.as_view(), name="cliente_create"),
+    path("cliente/list/", views.ClienteList.as_view(), name="cliente_list"),
+    path("cliente/update/<int:pk>", views.ClienteUpdate.as_view(), name="cliente_update"),
+    path("cliente/delete/<int:pk>", views.ClienteDelete.as_view(), name="cliente_delete"),
+    path("cliente/detail/<int:pk>", views.ClienteDetail.as_view(), name="cliente_detail"),
     path("pedidos/", views.pedidos, name="pedidos"),
     path("crear_pedidos/", views.crear_pedido, name="crear_pedidos"),
 
--- project/cliente/views.py
+++ project/cliente/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render, redirect
 from . import models, forms
+from django.views.generic import CreateView,DeleteView, DetailView, ListView, UpdateView
+from django.urls import reverse_lazy
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
-    consulta= request.GET.get("consulta", None)
-    if consulta:
-        consulta_clientes=models.Cliente.objects.filter(nombre__icontains=consulta)
-        if consulta_clientes.count() == 0: #si no encuentra al cliente por nombre lo busca por apellido
-            consulta_clientes=models.Cliente.objects.filter(apellido__icontains=consulta)
-    else:
-        consulta_clientes = consulta_clientes = models.Cliente.objects.all()
-    context = {"clientes": consulta_clientes}
-    return render(request, "cliente/index.html", context)
+    return render(request, "cliente/index.html")
 
-
+'''
 def crear_cliente(request):
     if request.method == "POST":
         form = forms.ClienteForm(request.POST)
@@ -24,7 +19,7 @@ def crear_cliente(request):
         form = forms.ClienteForm()
     return render(request, 'cliente/crear_cliente.html', {'form': form})
 
-
+'''
 def pedidos(request):
     consulta= request.GET.get("consulta", None)
     if consulta:
@@ -46,3 +41,37 @@ def crear_pedido(request):
     else:
         form = forms.PedidoForm()
     return render(request, 'cliente/crear_pedido.html', {'form': form})
+   
+######### CLASES DE CLIENTES #########
+
+class ClienteList(ListView):
+    model = models.Cliente
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        consulta = self.request.GET.get('consulta')  
+
+        if consulta:  
+            queryset = queryset.filter(
+                Q(nombre__icontains=consulta) |  
+                Q(apellido__icontains=consulta)  
+            )
+        return queryset
+    
+class ClienteDetail(DetailView):
+    model = models.Cliente
+
+class ClienteDelete(DeleteView):
+    model = models.Cliente
+    success_url = reverse_lazy("cliente:cliente_list")
+
+class ClienteCreate(CreateView):
+    model = models.Cliente
+    form_class = forms.ClienteForm
+    success_url = reverse_lazy("cliente:cliente_list")
+
+class ClienteUpdate(UpdateView):
+    model = models.Cliente
+    form_class = forms.ClienteForm
+    template_name = "cliente/cliente_update.html"
+    success_url = reverse_lazy("cliente:cliente_list")
