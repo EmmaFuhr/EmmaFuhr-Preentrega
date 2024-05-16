4313ef16cc2307cef58680c2422cfd83cc982237
Emma Fuhr <emma.a.fuhr@gmail.com>
2024-05-16 09:27:59 -0300

    add: CRUD de articulos

--- project/articulo/urls.py
+++ project/articulo/urls.py
@@ -5,7 +5,10 @@ app_name = 'articulo'
 
 urlpatterns = [
     path("articulo/", views.home, name="home"),
-    path("articulo/create/", views.articulo_create, name="articulo_create"),
-    path("articulo/list/", views.articulo_list, name="articulo_list"),
+    path("articulo/create/", views.ArticuloCreate.as_view(), name="articulo_create"),
+    path("articulo/list/", views.ArticuloList.as_view(), name="articulo_list"),
+    path("articulo/detail/<int:pk>", views.ArticuloDetail.as_view(), name="articulo_detail"),
+    path("articulo/update/<int:pk>", views.ArticuloUpdate.as_view(), name="articulo_update"),
+    path("articulo/delete/<int:pk>", views.ArticuloDelete.as_view(), name="articulo_delete"),
 
 ]
--- project/articulo/views.py
+++ project/articulo/views.py
@@ -1,26 +1,31 @@
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView,DeleteView, DetailView, ListView, UpdateView
 from . import models, forms
+from django.urls import reverse_lazy
 # Create your views here.
 def home(request):
     return render(request, "articulo/index.html")
 
-def articulo_list(request):
-    consulta= request.GET.get("consulta", None)
-    if consulta:
-        consulta_articulos=models.Articulo.objects.filter(nombre__icontains=consulta)
-    else:
-        consulta_articulos= models.Articulo.objects.all()
-    context = {"articulos": consulta_articulos}
-    return render(request, "articulo/articulo_list.html", context)
+class ArticuloList(ListView):
+    model = models.Articulo
+
+class ArticuloDetail(DetailView):
+    model = models.Articulo
+
+class ArticuloDelete(DeleteView):
+    model = models.Articulo
+    success_url = reverse_lazy("articulo:articulo_list")
+
+class ArticuloCreate(CreateView):
+    model = models.Articulo
+    form_class = forms.ArticuloForm
+    success_url = reverse_lazy("articulo:articulo_list")
+
+class ArticuloUpdate(UpdateView):
+    model = models.Articulo
+    form_class = forms.ArticuloForm
+    success_url = reverse_lazy("articulo:articulo_list")
 
 
-def articulo_create(request):
-    if request.method == "POST":
-        form = forms.ArticuloForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('articulo:home')
-    else:
-        form = forms.ArticuloForm()
-    return render(request, 'articulo/articulo_create.html', {'form': form})
+
+
