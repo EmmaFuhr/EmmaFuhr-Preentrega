76ac3d19b34ebd68afc078af8d0b36c50f0a37c1
Emma Fuhr <emma.a.fuhr@gmail.com>
2024-05-17 12:31:21 -0300

    add: login/logout en navbar

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name ='home'),
     path('about/', views.about, name ='about'),
+    path('login/', views.CustomLoginView.as_view(), name ='login'),
+    path('logout/', LogoutView.as_view(template_name="core/logout.html"), name ='logout'),
+    
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import CustomAuthenticationForm
+from django.contrib.auth.views import LoginView
 
 
 # Create your views here.
@@ -7,4 +9,9 @@ def home(request):
 
 def about(request):
     return render (request, 'core/about.html')
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "core/login.html"
+
 
